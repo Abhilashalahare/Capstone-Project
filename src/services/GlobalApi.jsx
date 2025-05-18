b61753002dc8ece7984c1caa83054a97e2117fdf
Abhilashalahare <166977285+Abhilashalahare@users.noreply.github.com>
2025-05-18 18:42:55 +0530

    Update GlobalApi.jsx

--- src/services/GlobalApi.jsx
+++ src/services/GlobalApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const movieBaseUrl = "https://api.themoviedb.org/3";
-const api_key = "2ec0d66f5bdf1dd12eefa0723f1479cf";
+const api_key = "d6660860ae2cea88e1f7b3daa3f7cfc8";
 
 //https://api.themoviedb.org/3/trending/all/day?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf
 const getTrendingVideos = axios.get(
